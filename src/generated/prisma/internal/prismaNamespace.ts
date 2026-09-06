e471fff1ef18821dd962425fa6a82f613e28f434
Demetrio Teodoro <demetrio.teodoro@gmail.com>
2026-09-06 00:31:23 -0300

    chore: Regenera namespace interno do Prisma Client

--- src/generated/prisma/internal/prismaNamespace.ts
+++ src/generated/prisma/internal/prismaNamespace.ts
@@ -835,6 +835,7 @@ export const UserScalarFieldEnum = {
   name: 'name',
   email: 'email',
   password: 'password',
+  hideSensitiveValues: 'hideSensitiveValues',
   createdAt: 'createdAt',
   updatedAt: 'updatedAt'
 } as const
@@ -940,6 +941,13 @@ export type StringFieldRefInput<$PrismaModel> = FieldRefInputType<$PrismaModel, 
  * Reference to a field of type 'String[]'
  */
 export type ListStringFieldRefInput<$PrismaModel> = FieldRefInputType<$PrismaModel, 'String[]'>
+    
+
+
+/**
+ * Reference to a field of type 'Boolean'
+ */
+export type BooleanFieldRefInput<$PrismaModel> = FieldRefInputType<$PrismaModel, 'Boolean'>
     
 
 
